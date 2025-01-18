ebe4f8d13b81b03bf9f6c00d8bc7b4df93744c6d
Thamires Ribeiro <thamires.ribeiro@gentell.com>
2025-01-18 14:18:17 -0300

    CRUD completed

---
+++ Controllers/TextController.cs
@@ -0,0 +1,121 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Back.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Back.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TextController : ControllerBase
+    {
+        public string cacheKey { get; set; }
+        private readonly IMemoryCache _memoryCache;
+
+        public TextController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+            cacheKey = "ToDoList";
+        }
+        
+
+        [HttpGet("/get")]
+        public ActionResult Get() {
+            var ToDoList = GetToDoList();
+            return Ok(ToDoList);
+        }
+
+        [HttpPost("/insert")]
+        public ActionResult Post([FromBody] ListItemDto model) {            
+            var ToDoList = GetToDoList();
+            ToDoList.Add(model);
+            _memoryCache.Set(cacheKey, ToDoList);
+
+            return Ok("Text inserted");
+        }
+
+        [HttpPut("/update/{index}")]
+        public ActionResult Update([FromRoute] int index, [FromBody] ListItemDto novaModel) {
+            var ToDoList = GetToDoList();
+
+            if (index < 0 || index >= ToDoList.Count) {
+                return BadRequest("Index invalid.");
+            }
+
+            ToDoList[index] = novaModel;
+            _memoryCache.Set(cacheKey, ToDoList);
+
+            return Ok("Text updated.");
+        }
+
+        [HttpDelete("/delete/{index}")]
+        public ActionResult Delete([FromRoute] int index) {
+            var ToDoList = GetToDoList();
+
+            if (index < 0 || index >= ToDoList.Count) {
+                return BadRequest("Index invalid.");
+            }
+            ToDoList.RemoveAt(index);
+            _memoryCache.Set(cacheKey, ToDoList);
+
+            return Ok("Text deleted.");
+        }
+
+        // request que filtra apenas os checkados e os nao checkados
+        [HttpGet("/filter")]
+        public ActionResult GetFilteredItems([FromQuery] string filter) {
+            var ToDoList = GetToDoList();
+
+            if (!string.IsNullOrEmpty(filter)) {
+                switch (filter) {
+                    case "checked":
+                        ToDoList = ToDoList.Where(item => item.Checked).ToList();
+                        break;
+                    case "unchecked":
+                        ToDoList = ToDoList.Where(item => !item.Checked).ToList();
+                        break;
+                    default:
+                        return BadRequest("Filter parameter invalid");
+ 
+                }
+            }
+
+            return Ok(ToDoList);
+        }
+
+        // [HttpGet("/filter")]
+        // public ActionResult Filter([FromQuery] bool? isChecked) {
+        //     var MinhaLista = GetMinhaLista();
+
+        //     if (isChecked.HasValue)
+        //     {
+        //         MinhaLista = MinhaLista.Where(item => item.Checked == isChecked.Value).ToList();
+        //     }
+
+        //     return Ok(MinhaLista);
+        // }
+
+        // autenticação jwt
+
+        private List<ListItemDto> GetToDoList() {
+            if (!_memoryCache.TryGetValue(cacheKey, out List<ListItemDto> ToDoList))
+            {
+                ToDoList = new List<ListItemDto>();
+                
+                // Set cache options
+                var cacheOptions = new MemoryCacheEntryOptions()
+                    .SetSlidingExpiration(TimeSpan.FromMinutes(5));
+
+                // Save data in cache
+                _memoryCache.Set(cacheKey, ToDoList, cacheOptions);
+            }
+
+            return ToDoList;
+        }
+        // esse private estaria em uma camada de service
+    }
+}
---
+++ Models/ListaGattoItemDto.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Back.Models
+//nome pro import
+{
+    public class ListItemDto
+    {
+        //so valor e a propriedade checked
+        public string Value { get; set; }
+        public bool Checked { get; set; }
+    }
+}
